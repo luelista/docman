ab74de16bbfbb6c1cb92b73de942acd553c3b165
Johannes Lauinger <johannes@lauinger-it.de>
2017-03-20 00:17:39 +0100

    fix bug in migration: no primary key

--- database/migrations/2017_03_19_023020_create_documenttags_table.php
+++ database/migrations/2017_03_19_023020_create_documenttags_table.php
@@ -13,7 +13,7 @@ class CreateDocumentTagsTable extends Migration
     public function up()
     {
         Schema::create('document_tags', function (Blueprint $table) {
-            $table->increments('id');
+            $table->integer('id');
             $table->string('tag');
         });
     }
