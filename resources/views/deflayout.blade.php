41ec4c2c3feb6bd1f9e1a3801b53b490017215ed
luelista <it@luelista.net>
2017-03-28 17:48:41 +0200

    allow installation in sub paths + design optimizations

--- resources/views/deflayout.blade.php
+++ resources/views/deflayout.blade.php
@@ -4,20 +4,20 @@
     <meta charset="utf-8">
     <title>docman</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://static.luelista.net/bootstrap-3.3.2-dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/style/style.css">
+    <link rel="stylesheet" href="{{ url('style/css/bootstrap-yeti-theme.min.css') }}">
+    <link rel="stylesheet" href="{{ url('style/style.css') }}">
     <script>csrf="{{ csrf_token() }}"</script>
     <script src="https://static.luelista.net/jquery/jquery-2.1.3.min.js"></script>
-    <script src="/style/helpers.js"></script>
+    <script src="{{ url('style/helpers.js') }}"></script>
 </head>
   <body>
-<header>
+<nav class="navbar navbar-default">
 <div class="container">
-<a href="/" class="brand">Dokumentenverwaltung</a>
- &nbsp; &nbsp; 
+<div class="navbar-header"><a href="/" class="navbar-brand">Dokumentenverwaltung</a></div>
+
  @yield("toolbar")
 </div>
-</header>
+</nav>
 
 <div class="container">
 @yield("main")
