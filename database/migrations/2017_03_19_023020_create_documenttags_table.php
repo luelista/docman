d159d95cb2f38b7e2ec07522013df781744ea4b4
Max Weller <igitt@max-weller.de>
2018-10-07 00:15:32 +0200

    fix migration

--- database/migrations/2017_03_19_023020_create_documenttags_table.php
+++ database/migrations/2017_03_19_023020_create_documenttags_table.php
@@ -13,8 +13,9 @@ class CreateDocumentTagsTable extends Migration
     public function up()
     {
         Schema::create('document_tags', function (Blueprint $table) {
-            $table->integer('id');
+            $table->integer('document_id')->unsigned();
             $table->string('tag');
+            $table->foreign('document_id')->references('id')->on('documents');
         });
     }
 
